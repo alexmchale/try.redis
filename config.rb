e505674d0c2a887e04729cdb279da281a95c078a
Jan-Erik Rediger <badboy@archlinux.us>
2013-08-03 21:45:58 +0200

    Handle logging with systemd (stdout & stderr)

--- config.rb
+++ config.rb
@@ -6,16 +6,13 @@ puma_pid        = "#{shared_path}/pids/puma.pid"
 puma_sock       = "unix://#{shared_path}/sockets/puma.sock"
 puma_control    = "unix://#{shared_path}/sockets/pumactl.sock"
 puma_state      = "#{shared_path}/sockets/puma.state"
-puma_log_stdout = "#{shared_path}/log/puma-#{stage}-stdout.log"
-puma_log_stderr = "#{shared_path}/log/puma-#{stage}-stderr.log"
 
 directory '/home/tryredis/try.redis/current'
 rackup 'config.ru'
 environment stage
-daemonize true
+daemonize false
 pidfile puma_pid
 state_path puma_state
-stdout_redirect puma_log_stdout, puma_log_stderr
 
 threads 2, 4
 
--- config.ru
+++ config.ru
@@ -5,13 +5,6 @@ REDIS_PORT = ENV['REDIS_PORT']
 
 require "./try-redis.rb"
 
-log = ::File.new(::File.join(::File.dirname(__FILE__),'log','sinatra.log'), "a")
-
-if production?
-  STDOUT.reopen(log)
-  STDERR.reopen(log)
-end
-
 use Rack::Static, :urls => %w( /css /images /javascripts ), :root => "public"
 
 run TryRedis
